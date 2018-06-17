55294e47f0871fed7a086263dd93b7cbe8c5cd8f
David <dodunayo@nd.edu>
2018-06-17 01:23:58 +0000

    The video is saved in .h264 format. working on the conversion to mp4 feature

--- RasPi/OnEdge.py
+++ RasPi/OnEdge.py
@@ -28,6 +28,13 @@ videoStream = None
 ### Working Directory
 SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]
 
+def run_shell(cmd):
+    """
+    Used for running shell commands
+    """
+    output = subprocess.check_output(cmd.split(' '))
+    return str(output.rstrip().decode())
+
 def start_camera():
     global cameraDevice
     global videoStream
@@ -38,13 +45,16 @@ def start_camera():
     #This might need to be negative
     global preroll
     preroll = 3
+    ### HUB Manager This
+    global captureRate 
+    captureRate = 30.0
     with picamera.PiCamera() as cameraDevice:
         #Add a check for cameraDevice here
         cameraDevice.start_preview()
         try:
             #This can be set from a Hub Manager
             cameraDevice.resolution = (1280, 720) #This needs to be a set to a resolution that the model can understand: I think (256,256)
-            cameraDevice.framerate = 30
+            cameraDevice.framerate = captureRate
             videoStream = picamera.PiCameraCircularIO(cameraDevice, seconds=captureTime + preroll)
             cameraDevice.start_recording(videoStream, format='h264')
             time.sleep(captureTime)
@@ -92,17 +102,28 @@ def get_video():
         os.makedirs(videoDirPath)
 
     videoStartTime =     startTime - timedelta(seconds=preroll)
-    h264FileName =       "video-{0}-{1}.h264".format(1,videoStartTime.strftime("%Y%m%d%H%M%S"))
+    h264FileName =       "video-{0}-{1}.h264".format("seun",videoStartTime.strftime("%Y%m%d%H%M%S"))
     h264FilePath =       "{0}/{1}/{2}".format(baseDir,videoDir,h264FileName)
     mp4FileName =        h264FileName.replace('.h264','.mp4')
     mp4FilePath =        "{0}/{1}/{2}".format(baseDir,videoDir,mp4FileName)
-    mp4FilePathTemp =   "{0}.tmp".format(mp4FileName)
+    mp4FilePathTemp =    "{0}.tmp".format(mp4FilePath)
     mp4BlobName =        "{0}/{1}".format(videoDir,mp4FileName)
     jsonFileName =       "{0}.json".format(mp4FileName)
     jsonFilePath =       "{0}/{1}/{2}".format(baseDir,videoDir,jsonFileName)
 
+    ##Save the video to a file path specified
     videoStream.copy_to(h264FilePath, seconds=captureTime + preroll)
-        ##Save the video to a filee next
+
+    #Convert to MP4 format for viewing
+    mp4box = "MP4Box -fps {0} -quiet -add {1} {2}".format(captureRate,h264FilePath,mp4FilePathTemp)
+    print("Running the MP4BOX")
+    print(h264FilePath)
+    print(mp4FilePathTemp)
+    #run_shell(mp4box)
+
+    #os.rename(mp4FilePathTemp, mp4FilePath)
+    print("Finished get_video")
+
 
 def main():
     print("Welcome to main")
