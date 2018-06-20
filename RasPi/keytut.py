f2e5bc7fffefd06e818621e33490186326390cfa
David Odun-Ayo <t-daodu@microsoft.com>
2018-06-20 12:27:13 -0700

    Modified and combined versions of the video buffer

--- RasPi/keytut.py
+++ RasPi/keytut.py
@@ -14,6 +14,11 @@ from PIL import Image
 from datetime import datetime, timedelta
 
 #Define Variables
+global camera_device
+global video_stream
+global capture_time
+global preroll
+global capture_rate
 
 
 ## Get Working Directory
@@ -31,34 +36,114 @@ def getch():
     return ch
 
 ## Function for running specific shell commands
+def run_shell(cmd):
+    """
+    Used for running shell commands
+    """
+    output = subprocess.check_output(cmd.split(' '))
+    return str(output.rstrip().decode())
 
+def start_camera():
+    ##Define Variables
+    capture_rate = 30 #FPS
+    capture_time = 30 #Seconds
+    with picamera.PiCamera() as camera_device:
+        camera_device.start_preview()
+        try:
+            camera_device.resolution = (1280, 720)
+            camera_device.framerate = capture_rate
+            video_stream = picamera.PiCameraCircularIO(camera_device, capture_time)
+            camera_device.start_recording(video_stream, format='h264')
+        except Exception as ex:
+            camera_device = None
+            video_stream = None
 
-camera = picamera.PiCamera()
-stream = picamera.PiCameraCircularIO(camera, seconds=20)
-camera.start_recording(stream, format='h264')
-camera.start_preview()
-bDelay = 0.2
-try:
-    while True:
-        char = getch()
-        camera.wait_recording(1)
+def stop_camera():
+    if(camera_device is not None):
+        try:
+            camera_device.close()
+            camera_evice = None
+            if (video_stream is not None):
+                video_stream.close()
+                video_stream = None
+        except Exception as ex:
+            # print("Camera and Buffer failed to close") make a log statement
+            camera_device = None
+            video_stream = None
 
-        if (char == "p"):
-            print("Stop")
+def get_video():
+    ## Start the camera and begin recording for video
+    start_camera()
+
+    if camera_device is None:
+        ## Print Log Error
+        return
+
+    get_key = True
+    capture_video = False
+    buttonDelay = 0.2 #Seconds
+    preroll = 7 # Seconds to go back before an event
+    
+    ## Create a folder to save the videos
+    start_time = datetime.now()
+    base_dir = SCRIPT_DIR
+    video_dir = "myvideos"
+    video_dir_path = "{0}/{1}".format(base_dir,video_dir)
+
+    if not os.path.exists(video_dir_path):
+        os.makedirs(video_dir_path)
+    
+    video_start_time =     start_time - timedelta(seconds=preroll)
+    ## Set up seperate paths for before and after the event
+    #Before:
+    before_event =         "video-{0}-{1}.h264".format("before",video_start_time.strftime("%Y%m%d%H%M%S"))
+    before_event_path =    "{0}/{1}/{2}".format(base_dir,video_dir,before_event)
+    before_mp4 =           before_event.replace('.h264','.mp4')
+    before_mp4_path =      "{0}/{1}/{2}".format(base_dir,video_dir,before_mp4)
+    before_path_temp =      "{0}.tmp".format(before_mp4_path)
+
+    #After:
+    after_event =         "video-{0}-{1}.h264".format("after",video_start_time.strftime("%Y%m%d%H%M%S"))
+    after_event_path =    "{0}/{1}/{2}".format(base_dir,video_dir, after_event)
+    after_mp4 =           after_event.replace('.h264','.mp4')
+    after_mp4_path =      "{0}/{1}/{2}".format(base_dir,video_dir,after_mp4)
+    after_path_temp =     "{0}.tmp".format(after_mp4_path)
+    try:
+        while (get_key == True):
+            char = getch()
+            camera_device.wait_recording(1)
+
+            if (char == "p"):
+            ##Change to log debiugs
             exit(0)
         
-        if (char == "s"):
-            #camera.wait_recording(10)
-            #stream.copy_to('video.h264')
-            #time.sleep(bDelay)
-            camera.split_recording('after.h264')
-            stream.copy_to('before.h264', seconds = 10)
-            camera.wait_recording(10)
-            exit(0)
-        elif (char == "a"):
-            print("letter a was pressed")
-            time.sleep(bDelay)
-finally:
-    print("I am in finally")
-    camera.stop_recording()
+            if (char == "s"):
+                capture_video = True
+                time.sleep(button_delay)
+                get_key == False
+            else
+                get_key == True
+    except Exception as ex:
+            camera_device = None
+            video_stream = None
 
+    if (capture_video == True):
+        camera_device.split_recording(after_event_path) #We know it is an event so continue recording
+        video_stream.copy_to(before_event_path, seconds = preroll)
+        camera_device.wait_recording(20) #Continue recording to the after path for 20 seconds
+        
+        mp4box_before = "MP4Box -fps {0} -quiet -add {1} {2}".format(captureRate,before_event_path,before_path_temp) 
+        mp4box_after  = "MP4Box -fps {0} -quiet -add {1} {2}".format(captureRate,after_event_path,after_path_temp)
+
+        run_shell(mp4box_before)
+        run_shell(mp4box_after)
+        os.remove(before_event_path)
+        os.remove(after_event_path)
+        
+def main():
+    get_video()
+
+
+
+if __name__ == '__main__':
+    main()
