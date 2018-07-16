6bd571b321094389f9646fe8fed22c2599fce666
David <dodunayo@nd.edu>
2018-07-16 22:12:31 +0000

    Deleted the Git ignore and I am adding pack the pi3 folder

---
+++ RasPi/pi3/build/CMakeFiles/_model.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/Raspi/MLontheEdge/RasPi/pi3/modelPYTHON_wrap.cxx" "/home/pi/Desktop/Raspi/MLontheEdge/RasPi/pi3/build/CMakeFiles/_model.dir/modelPYTHON_wrap.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SWIG_PYTHON_INTERPRETER_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/python3.5m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
