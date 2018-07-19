a3372577107c33668987178714cf542252f4f4ce
David <dodunayo@nd.edu>
2018-07-19 20:30:27 +0000

    Fixed the scripting structure of the Host.py script and make it a lot more readible

--- RasPi/Host.py
+++ RasPi/Host.py
@@ -18,27 +18,25 @@ def azure_upload_from_path(blob_container,blob_name,blob_object,blob_format):
 def main():
     # Define Globals
     global block_blob_service
-
-    block_blob_service = BlockBlobService(account_name='*************', account_key='***************************************')
-    print("Blob Storage Worked")
     
-    model_container_name = 'edgemodels2'
-    block_blob_service.create_container(model_container_name)
-    print("Model Created")
-
+    # Define Locals
     model_dir = "pi3"
     model_dir_path = "{0}/{1}".format(SCRIPT_DIR, model_dir)
     compressed_model_name = "zipped{0}".format(model_dir)
+    model_container_name = 'edgemodels'
     compressed_model_dir_path ="{0}/{1}.zip".format(SCRIPT_DIR, compressed_model_name)
+    
+    # Initialize Azure Connection
+    block_blob_service = BlockBlobService(account_name='*************', account_key='***************************************')
+    
+    # Create Blob Container if it doesn't already exists
+    block_blob_service.create_container(model_container_name)
 
-    print("The Archive is about to be compressed")
-    shutil.make_archive(compressed_model_name,'zip',model_dir_path)
-    print("Finished compressing the path")
+    # Compress the current model file on the computer
+    shutil.make_archive(compressed_model_name, 'zip', model_dir_path)
 
+    # Upload the Compressed file to Azure
     azure_upload_from_path(model_container_name, compressed_model_name, compressed_model_dir_path, 'application/zip')
-    print("The Upload Worked")
-
-    print("Hello World")
 
 if __name__ == '__main__':
     main()
