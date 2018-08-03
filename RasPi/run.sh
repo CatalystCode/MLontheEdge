f8b9c81fb2ab1f10c364eccda9179903781277ef
David Odun-Ayo <dodunayo@nd.edu>
2018-08-03 00:22:29 +0000

    Got the dockerfile building and running shell and python scripts

---
+++ RasPi/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONPATH=$PYTHONPATH:/home/pi/local/lib/python3.5/site-packages
+python3 ./test.py
---
+++ RasPi/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import cv2
+import io
+import json
+import logging
+import numpy as numpy
+import os
+import picamera
+import random
+import shutil
+import subprocess
+import sys
+import termios
+import time
+import tty
+import zipfile
+from datetime import datetime, timedelta
+#from azure.storage.blob import BlockBlobService, ContentSettings, PublicAccess
+# Import the right folder and file
+#import ellmanager as emanager
+#import model
+
+def main():
+    while True:
+        print('This is a test that worked')
+
+
+if __name__ == '__main__':
+    main()
