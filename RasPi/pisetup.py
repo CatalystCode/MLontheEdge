7697c262b38da25d6a65fadb7b1e55a5865ac254
David <dodunayo@nd.edu>
2018-08-14 02:04:53 +0000

    Added the working pisetup.py script that downloads the updated pi3 folder and the categories.txt file for use

--- RasPi/pisetup.py
+++ RasPi/pisetup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-# Description:
-# 1. Delete the current version of pi3: Check 
-# 2. Download the latest version froma Azure: Check
-# 3. Run Cmake: Check
-# 4. Run Make: Check
-# 5. Go back to the Edge.py script
+###############################################################################
+#
+#  Project:  MLontheEdgeCodeStory
+#  File:     pisetup.py
+#  Authors:  David (Seun) Odun-Ayo
+#  Description: This script gets the latest version of the pi3 folder  and 
+#   categories.txt from azure blob storage and then makes it for use on the 
+#   project 
+#  Requires: Python 3.x
+#
+###############################################################################
 
 import logging
 import os
@@ -17,12 +22,19 @@ from azure.storage.blob import BlockBlobService, ContentSettings, PublicAccess
 SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]
 
 def azure_download_from_path(model_container_name, model_dir_path, compressed_model_dir_path, compressed_model_name):
+    # Define categories variables and get categories.txt
+    categories_dir = "categories.txt"
+    categories_path = "{0}/{1}".format(SCRIPT_DIR, categories_dir)
+    block_blob_service.get_blob_to_path(model_container_name, categories_dir, categories_path)
+        
     #Download Azure Version to the Raspberry pi
-        block_blob_service.get_blob_to_path(model_container_name, compressed_model_name, compressed_model_dir_path)
-        os.makedirs(model_dir_path)
-        zf = zipfile.ZipFile(compressed_model_dir_path)
-        zf.extractall(model_dir_path)
-        os.remove(compressed_model_dir_path)
+    block_blob_service.get_blob_to_path(model_container_name, compressed_model_name, compressed_model_dir_path)
+    os.makedirs(model_dir_path)
+    zf = zipfile.ZipFile(compressed_model_dir_path)
+    zf.extractall(model_dir_path)
+    os.remove(compressed_model_dir_path)
+
+
 
 def main():
     # Define Globals
@@ -37,16 +49,25 @@ def main():
     build_dir_path = "{0}/{1}".format(model_dir_path, build_dir)
     compressed_model_name = "zipped{0}".format(model_dir)
     compressed_model_dir_path ="{0}/{1}.zip".format(SCRIPT_DIR, compressed_model_name)
+
+    # Intialize Log Properties
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s') 
    
     # Get Login Credentials
     azure_key_name = os.environ.get('AZURE_BLOBCONTAINER_NAME')
     azure_key = os.environ.get('AZURE_BLOBCONTAINER_KEY')
-    
-    if azure_key_name or azure_key is None:
-        logging.debug('Error Loading Azure Blob Storage Keys. Exiting...')
+
+    if azure_key_name is None:
+        logging.debug('Name Error Failed. Exiting....')
         sys.exit(1)
-    else:
-        logging.debug('Azure Storage Login Successful')
+
+    if azure_key is None:
+        logging.debug('Key Error Failed. Exiting....')
+        sys.exit(1)
+
+    if azure_key and azure_key_name is not None:
+        logging.debug('Everything worked fine')
+
         
     # Set up Azure Credentials
     block_blob_service = BlockBlobService(account_name = azure_key_name, account_key = azure_key)
